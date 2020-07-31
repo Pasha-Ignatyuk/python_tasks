70d2331064ae469361f1e053a199eb2a6dd22464
Pasha-Ignatyuk <68711099+Pasha-Ignatyuk@users.noreply.github.com>
2020-07-31 15:28:38 +0300

    Update task_3__1.py

---
+++ task_3__1.py
@@ -0,0 +1,7 @@
+number = int(input('enter number'))
+remainder = number % 1000
+if remainder == 0:
+    print('millenium')
+else:
+    pass
+# in the task if the condition is not met, nothing is displayed
