80bc4e791e6edd886447cccf86dad4b507b4b4ff
Pasha-Ignatyuk <68711099+Pasha-Ignatyuk@users.noreply.github.com>
2020-08-08 13:03:18 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -13,4 +13,18 @@ def factorial():
     print(fact)
     return fact
 
+
 factorial()
+
+#  рекурсия
+number = int(input('enter a positive integer'))
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(number))
